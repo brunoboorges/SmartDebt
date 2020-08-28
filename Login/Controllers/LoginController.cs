aaac456fc77c985a1df0934078e77cabc351b3f4
Bruno Borges <brunotomo@gmail.com>
2020-08-28 09:14:38 -0300

    Projeto com Login/SignUp e Tela Inicial

---
+++ Login/Controllers/LoginController.cs
@@ -0,0 +1,96 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult Login(Usuario user)
+        {
+            Usuario login = new Usuario();
+
+            if (login.Login(user))
+            { 
+                Session["usuarioLogadoID"] = user.Nome;
+                return View("App");
+            }
+            else { 
+            return View("Error");
+                
+            }
+        }
+        public ActionResult Teste()
+        {
+            if (Session["usuarioLogadoID"] != null) { 
+
+                return View();
+            }
+            return Redirect("Login");
+        }
+
+        public ActionResult Exemplo()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SignUp(Usuario user)
+        {
+            Usuario signUp = new Usuario();
+            if (signUp.SignUp(user) == 2)
+            {
+                ViewBag.Message = "Usuario cadastrado com sucesso";
+            }
+            else if (signUp.SignUp(user) == 1)
+            {
+                ViewBag.Message = "Este usuário já existe, por favor tente outro nome de usuário.";
+            }
+            else if (signUp.SignUp(user) == 3)
+            {
+                ViewBag.Message = "Senhas não combinam, por favor tente novamente.";
+            }
+            else if (signUp.SignUp(user) == 4)
+            {
+                ViewBag.Message = "Usuario precisa ter mais do que 5 caracteres.";
+            }
+            else if (signUp.SignUp(user) == 5)
+            {
+                ViewBag.Message = "Senha precisa ter mais do que 5 caracteres.";
+            }
+
+            return View();
+        }
+
+        public ActionResult App()
+        {
+            if (Session["usuarioLogadoID"] != null){
+                return View();
+            }else
+            return Redirect("Login");
+        }
+    }
+}
---
+++ Login/Models/Usuario.cs
@@ -0,0 +1,162 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Models
+{
+    public class Usuario
+    {
+        string connectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Login"].ConnectionString;
+        }
+
+        
+        
+        public string Nome { get; set; }
+        public string Password { get; set; }
+        public string SecondPass { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(string nome, string password)
+        {
+            Nome = nome;
+            Password = password;
+        }
+
+
+        
+
+        //CADASTRAR ----------------------------------------------------
+        public int SignUp(Usuario user)
+        {
+
+            SqlConnection con = new SqlConnection(connectionString());
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+
+            try
+            {
+                cmd.Connection = con;
+                con.Open();
+
+                string firstPass = user.Password;
+                string secondPass = user.SecondPass;
+                string login = "";
+
+                cmd.CommandText = "select * from usuarios where usuario='" + user.Nome + "'";
+
+
+                dr = cmd.ExecuteReader();
+
+
+                if (dr.Read())
+                {
+                    login = dr["usuario"].ToString();
+                    
+                }
+                if (user.Nome.Length < 5)
+                {
+                    return 4;
+                }
+                dr.Dispose();
+                //SE JA EXISTIR USUARIO, RETORNE: "JÁ EXISTE ESTE NOME DE USUARIO."
+                if (login == user.Nome)
+                {
+                    return 1;
+                }
+
+                else { 
+                
+                if(firstPass.Length < 5)
+                    {
+                        return 5;
+                    }
+
+                if (firstPass == secondPass)
+                {
+                    
+                    cmd.CommandText = "insert into usuarios (usuario, senha) values ('" + user.Nome + "', '" + user.Password + "' )";
+                    dr = cmd.ExecuteReader();
+                        //SE LOGIN E SENHA PASSAR RETORNE: "USUARIO CADASTRADO COM SUCESSO"!
+                    return 2;
+                       
+                    }
+                else
+                {
+                        //SE SENHA NÃO PASSAR, RETORN: "SENHAS NÃO COMBINAM"
+                        return 3;
+                        
+                    }
+                }
+                
+
+            }
+
+            
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return 4;
+        }
+        
+
+        //LOGAR ----------------------------------------------------
+        public bool Login(Usuario user)
+        {
+
+            SqlConnection con = new SqlConnection(connectionString());
+            SqlCommand cmd = new SqlCommand();  
+            SqlDataReader dr;
+
+            string userDb = "";
+            string senhaDb = "";
+ 
+            try
+            {
+                
+
+                cmd.Connection = con;
+                con.Open();
+
+                cmd.CommandText = "select * from usuarios where usuario='" + user.Nome + "' and senha='" + user.Password + "'";
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    userDb = dr["usuario"].ToString();
+                    senhaDb = dr["senha"].ToString();
+                }
+                if (user.Nome == userDb && user.Password == senhaDb)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return false;
+        }
+
+        
+    }
+}
